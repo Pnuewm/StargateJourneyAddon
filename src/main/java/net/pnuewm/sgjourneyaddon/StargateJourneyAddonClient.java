52c07fd527f91c9bf95ca6a6bfb25572dd8b28e1
Pnuewm <dscalzi92@gmail.com>
2026-08-24 16:41:30 -0400

    Initial Commit
    This starting point is the untouched template created by Neoforge.

---
+++ src/main/java/net/pnuewm/sgjourneyaddon/StargateJourneyAddonClient.java
@@ -0,0 +1,31 @@
+package net.pnuewm.sgjourneyaddon;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = StargateJourneyAddon.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = StargateJourneyAddon.MODID, value = Dist.CLIENT)
+public class StargateJourneyAddonClient {
+    public StargateJourneyAddonClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        StargateJourneyAddon.LOGGER.info("HELLO FROM CLIENT SETUP");
+        StargateJourneyAddon.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
