8fcce4c0642caaf4fd78796c462c686bed18c063
Pnuewm <dscalzi92@gmail.com>
2026-09-04 21:47:10 -0400

    Remove Template Java Programs

--- src/main/java/net/pnuewm/sgjourneyaddon/StargateJourneyAddonClient.java
+++ src/main/java/net/pnuewm/sgjourneyaddon/StargateJourneyAddonClient.java
@@ -10,22 +10,22 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-// This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = StargateJourneyAddon.MODID, dist = Dist.CLIENT)
-// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = StargateJourneyAddon.MODID, value = Dist.CLIENT)
-public class StargateJourneyAddonClient {
-    public StargateJourneyAddonClient(ModContainer container) {
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
-        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-    }
+// // This class will not load on dedicated servers. Accessing client side code from here is safe.
+// @Mod(value = StargateJourneyAddon.MODID, dist = Dist.CLIENT)
+// // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+// @EventBusSubscriber(modid = StargateJourneyAddon.MODID, value = Dist.CLIENT)
+// public class StargateJourneyAddonClient {
+    // public StargateJourneyAddonClient(ModContainer container) {
+        // // Allows NeoForge to create a config screen for this mod's configs.
+        // // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // // Do not forget to add translations for your config options to the en_us.json file.
+        // container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    // }
 
-    @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        StargateJourneyAddon.LOGGER.info("HELLO FROM CLIENT SETUP");
-        StargateJourneyAddon.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-    }
-}
+    // @SubscribeEvent
+    // static void onClientSetup(FMLClientSetupEvent event) {
+        // // Some client setup code
+        // StargateJourneyAddon.LOGGER.info("HELLO FROM CLIENT SETUP");
+        // StargateJourneyAddon.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    // }
+// }
